241b561f67a399a1556956f53603426c30d0912d
rerorero987 <rerorero987@kxf.biglobe.ne.jp>
2021-12-29 01:03:23 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import time
+
+st.title('Streamlit 超入門')
+
+st.write('プログレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i + 1}')
+    bar.progress(i + 1)
+    time.sleep(0.05)
+
+'Done!!!!'
+
+left_column , right_column = st.beta_columns(2)
+
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラムです')
+
+expander1 = st.beta_expander('問い合わせ1')
+expander1.write('問い合わせ1の回答')
+expander2 = st.beta_expander('問い合わせ2')
+expander2.write('問い合わせ2の回答')
+expander3 = st.beta_expander('問い合わせ3')
+expander3.write('問い合わせ3の回答')
+
+
+# text = st.text_input('あなたの趣味を教えてください。')
+# condition = st.slider('あなたの今の調子は',0,100,50)
+# f'あなたの好きな趣味は、{text}です'
+# f'あなたの調子は：{condition}'
+
+# if st.checkbox('Show Image'):
+#     img = Image.open('IIFD3378.JPEG')
+#     st.image(img,caption='Akama Minoru',use_column_width=True)
+
